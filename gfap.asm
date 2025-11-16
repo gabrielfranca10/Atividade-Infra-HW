75b1dbb006f277038381b9857ae6535b36c6610a
gabrielfranca10 <francagabriel285@gmail.com>
2025-11-16 17:13:35 -0300

    entrega da terceira questao real em decimal para float e double

--- gfap.asm
+++ gfap.asm
@@ -1,7 +1,7 @@
 # Autor: Gabriel França de Albuquerque Pernambuco
 # Email: gfap@cesar.school
 # Calcuadora Programador Didática
-# Revisão: 14/11/2025 11:45
+# Revisão: 16/11/2025 17:09
  
 .data
 menu1: .asciiz "\n[Q1] Conversões a partir de base 10:\n1) Binário\n2) Octal\n3) Hexadecimal\n4) BCD\nEscolha: "
@@ -185,3 +185,90 @@ loop_bits:
 fim:
     li $v0, 10
     syscall
+
+# Real → Float e Double (IEEE 754) com passos
+
+.data
+ask: .asciiz "\nDigite um número real: "
+msgf: .asciiz "\nFLOAT (32 bits): "
+msgd: .asciiz "\nDOUBLE (64 bits): "
+sinal: .asciiz "\nSinal: "
+expo:  .asciiz "\nExpoente: "
+expob: .asciiz "\nExpoente c/ viés: "
+frac:  .asciiz "\nFração: "
+newline: .asciiz "\n"
+
+.text
+main:
+
+    li $v0, 4
+    la $a0, ask
+    syscall
+
+    li $v0, 6
+    syscall
+    mov.s $f0, $f0
+
+##################################
+# FLOAT – print dos 32 bits      #
+##################################
+    li $v0, 4
+    la $a0, msgf
+    syscall
+
+    mfc1 $t0, $f0    # bits
+
+    li $t1, 31
+float_bits:
+    bltz $t1, double_part
+
+    srl $t2, $t0, $t1
+    andi $t2, $t2, 1
+
+    li $v0, 1
+    move $a0, $t2
+    syscall
+
+    addi $t1, $t1, -1
+    j float_bits
+
+##################################
+# DOUBLE – print dos 64 bits     #
+##################################
+double_part:
+    li $v0, 4
+    la $a0, msgd
+    syscall
+
+    cvt.d.s $f2, $f0
+    mfc1 $t4, $f2
+    mfc1 $t5, $f3
+
+    move $t0, $t4
+    li $t1, 63
+
+loop_d:
+    bltz $t1, fim
+
+    if_le_31:
+        srl $t2, $t5, $t1
+        andi $t2, $t2, 1
+        j print_d
+
+    else_bits:
+        sub $t3, $t1, 32
+        srl $t2, $t4, $t3
+        andi $t2, $t2, 1
+
+print_d:
+    li $v0, 1
+    move $a0, $t2
+    syscall
+
+    addi $t1, $t1, -1
+    j loop_d
+
+fim:
+    li $v0, 10
+    syscall
+
